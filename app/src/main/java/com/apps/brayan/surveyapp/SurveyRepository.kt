e93746b48672e5de6d6a953a02dd8dc22c9644dd
bragonya <bragonya@gmail.com>
2019-01-26 22:18:52 -0600

    update dependences and firebase auth was included

--- app/src/main/java/com/apps/brayan/surveyapp/SurveyRepository.kt
+++ app/src/main/java/com/apps/brayan/surveyapp/SurveyRepository.kt
@@ -3,6 +3,7 @@ package com.apps.brayan.surveyapp
 import android.arch.lifecycle.MutableLiveData
 import android.content.Context
 import android.os.Handler
+import android.util.Log
 import com.apps.brayan.surveyapp.coreapp.Cache
 import com.apps.brayan.surveyapp.coreapp.NetworkManager
 import com.apps.brayan.surveyapp.coreapp.SessionManager
@@ -11,13 +12,17 @@ import com.apps.brayan.surveyapp.login.LoginCallback
 import com.apps.brayan.surveyapp.models.OrgDetail
 import com.apps.brayan.surveyapp.models.Survey
 import com.apps.brayan.surveyapp.models.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import java.net.ConnectException
+import android.widget.Toast
 
-class SurveyRepository(var firebaseDatabase: FirebaseDatabase, var context:MasterApp) {
+
+
+class SurveyRepository(var firebaseDatabase: FirebaseDatabase, var firebaseAuth: FirebaseAuth, var context:MasterApp) {
     var orgDetailDomain = "https://bdsurvey-4d97c.firebaseio.com/proyectos/{organization}"
     var surveysDomain = "$orgDetailDomain/encuestas"
     var usersDomain = "https://bdsurvey-4d97c.firebaseio.com/usuarios/"
@@ -91,35 +96,48 @@ class SurveyRepository(var firebaseDatabase: FirebaseDatabase, var context:Maste
         }
     }
 
-    fun userValidations(userId:String, password: String, callback:LoginCallback){
-        if(NetworkManager.isNetworkAvailable(context)) {
-            val finalUrl = usersDomain + userId
-            val myRef = firebaseDatabase.getReferenceFromUrl(finalUrl)
-            val postListener = object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if (dataSnapshot != null) {
-                        val user = dataSnapshot.getValue(User::class.java)
-                        if (user != null) {
-                            if (user.password.equals(password)) {
-                                user.id = userId
-                                SessionManager.createUser(context, user)
-                                callback.onFinishProcess(true)
-                            } else {
-                                callback.onFinishProcess(false)
-                            }
-                        } else {
-                            callback.onFinishProcess(false)
-                        }
+    private fun userValidations(userId:String,callback:LoginCallback){
+        val finalUrl = usersDomain + userId
+        val myRef = firebaseDatabase.getReferenceFromUrl(finalUrl)
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot != null) {
+                    val user = dataSnapshot.getValue(User::class.java)
+                    if (user != null) {
+                        user.id = userId
+                        SessionManager.createUser(context, user)
+                        callback.onFinishProcess(true)
                     } else {
                         callback.onFinishProcess(false)
                     }
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
+                } else {
                     callback.onFinishProcess(false)
                 }
             }
-            myRef.addValueEventListener(postListener)
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                callback.onFinishProcess(false)
+            }
+        }
+        myRef.addValueEventListener(postListener)
+
+    }
+
+    fun userValidations(email:String, password: String,callback:LoginCallback){
+        if(NetworkManager.isNetworkAvailable(context)) {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("authprocess", "signInWithEmail:success")
+                    val user = firebaseAuth.getCurrentUser()
+                    if(user!=null){
+                        userValidations(user.uid,callback)
+                    }else{
+                        callback.onFinishProcess(false)
+                    }
+                } else {
+                    callback.onFinishProcess(false)
+                }
+            }
         }else{
             callback.onFailure(ConnectException())
         }
--- app/src/main/java/com/apps/brayan/surveyapp/api/NetworkLayerModule.kt
+++ app/src/main/java/com/apps/brayan/surveyapp/api/NetworkLayerModule.kt
@@ -3,13 +3,14 @@ package com.apps.brayan.surveyapp.api
 import com.apps.brayan.surveyapp.SurveyRepository
 import com.apps.brayan.surveyapp.coreapp.application.MasterApp
 import com.apps.brayan.surveyapp.firebase.database.FirebaseModule
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import dagger.Module
 import dagger.Provides
 @Module(includes = [FirebaseModule::class])
 class NetworkLayerModule{
     @Provides
-    fun provideRepository(firebaseDatabase: FirebaseDatabase,context:MasterApp): SurveyRepository{
-        return SurveyRepository(firebaseDatabase,context)
+    fun provideRepository(firebaseDatabase: FirebaseDatabase,firebaseAuth: FirebaseAuth,context:MasterApp): SurveyRepository{
+        return SurveyRepository(firebaseDatabase,firebaseAuth,context)
     }
 }
