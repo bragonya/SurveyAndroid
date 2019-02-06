f6a866ee69410637ce070538329f013790d3a483
brayany <brayan.yaquian@fox.com>
2019-02-06 10:50:49 -0600

    fix database persistence crash

--- app/src/main/java/com/apps/brayan/surveyapp/coreapp/application/MasterApp.kt
+++ app/src/main/java/com/apps/brayan/surveyapp/coreapp/application/MasterApp.kt
@@ -6,12 +6,14 @@ import com.apps.brayan.surveyapp.coreapp.application.di.AppComponent
 import com.apps.brayan.surveyapp.coreapp.application.di.AppInjector
 import com.apps.brayan.surveyapp.coreapp.application.di.AppModule
 import com.apps.brayan.surveyapp.coreapp.application.di.DaggerAppComponent
+import com.google.firebase.database.FirebaseDatabase
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import javax.inject.Inject
 
 class MasterApp: Application(), HasActivityInjector {
+    var firebaseInstance:FirebaseDatabase?=null
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
     override fun activityInjector() = dispatchingAndroidInjector
@@ -20,7 +22,15 @@ class MasterApp: Application(), HasActivityInjector {
     override fun onCreate() {
         super.onCreate()
         AppInjector.init(this)
-    }
 
+        setupFirebaseDatabase()
+    }
+    fun setupFirebaseDatabase(){
+
+        if (firebaseInstance==null) {
+            firebaseInstance = FirebaseDatabase.getInstance()
+            firebaseInstance?.setPersistenceEnabled(true)
+        }
+    }
 
 }
